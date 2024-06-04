872d87c054d9790beb00b237e0bfa5159334e937
Rubayed All Islam (Munna) <66661757+RubayedMunna@users.noreply.github.com>
2024-06-04 23:29:58 +0600

    Delete backend directory

--- backend/routes/departmentRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { uploadDepartmentsAsXML } = require('../controllers/departmentController'); // Adjust the path if needed
-
-router.post('/upload-department', uploadDepartmentsAsXML);
-
-module.exports = router;
